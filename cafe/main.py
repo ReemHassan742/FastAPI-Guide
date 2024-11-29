a68758fb3a59fe183454682ead36984abe340bcd
Reem Hassan <reemhassan742r@gmail.com>
2024-11-29 23:52:34 +0200

    Add files via upload

---
+++ cafe/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List 
+from database import menu, orders, reviwes
+
+
+app = FastAPI()
+
+class MenuItems(BaseModel):
+    id: int
+    name:str
+    price: float
+    desqription: str
+    
+class Order(BaseModel):
+    id: int
+    items: List[int]
+    total: float = 0.0
+    
+class Review(BaseModel):
+    item_id: int
+    review: str
+    rating: int #From 1 to 5    
+
+#Endpoints
+
+@app.get("/menu", response_model=List[MenuItems])
+def get_menu():
+    return menu 
+
+@app.post("/menu")
+def add_menu_item(item: MenuItems):
+    if any(i.id == item.id for i in menu):
+        raise HTTPException(status_code=400, detail="Item with this ID already exists.")
+    menu.append(item)
+    return {"message":"Menu item added successfully!"}
+
+@app.get("/orders", response_model=List[Order])
+def get_orders():
+    return orders
+
+@app.post("/order")
+def place_order(order: Order):
+    total_price = sum(menu_item.price for menu_item in menu if menu_item.id in order.items)
+    order.total = total_price
+    orders.append(order)
+    return {"message": f"order placed successfully! Total: ${total_price}"}
+
+@add
