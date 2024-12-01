d6382a6abf40929d80f9a8a494b9d08eb069ef31
Reem Hassan <reemhassan742r@gmail.com>
2024-12-01 23:00:11 +0200

    Update main.py

--- cafe/main.py
+++ cafe/main.py
@@ -1,39 +1,33 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List 
-from database import menu, orders, reviwes
+from typing import List
+
+from database import menu, orders, reviews
+from models import MenuItem, Order, Review
 
 
-app = FastAPI()
+app = FastAPI(
+    docs_url="/docs", 
+    redoc_url="/redoc", 
+    openapi_url="/openapi.json"  
+)
 
-class MenuItems(BaseModel):
-    id: int
-    name:str
-    price: float
-    desqription: str
-    
-class Order(BaseModel):
-    id: int
-    items: List[int]
-    total: float = 0.0
-    
-class Review(BaseModel):
-    item_id: int
-    review: str
-    rating: int #From 1 to 5    
 
-#Endpoints
+# Endpoints
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Cafe API!"}
 
-@app.get("/menu", response_model=List[MenuItems])
+
+@app.get("/menu", response_model=List[MenuItem])
 def get_menu():
-    return menu 
+    return menu
 
 @app.post("/menu")
-def add_menu_item(item: MenuItems):
+def add_menu_item(item: MenuItem):
     if any(i.id == item.id for i in menu):
         raise HTTPException(status_code=400, detail="Item with this ID already exists.")
     menu.append(item)
-    return {"message":"Menu item added successfully!"}
+    return {"message": "Menu item added successfully!"}
 
 @app.get("/orders", response_model=List[Order])
 def get_orders():
@@ -44,15 +38,15 @@ def place_order(order: Order):
     total_price = sum(menu_item.price for menu_item in menu if menu_item.id in order.items)
     order.total = total_price
     orders.append(order)
-    return {"message": f"order placed successfully! Total: ${total_price}"}
+    return {"message": f"Order placed successfully! Total: ${total_price}"}
 
-@add.post("/reviews")
-def add_reveiw(review: Review):
+@app.post("/reviews")
+def add_review(review: Review):
     if not any(item.id == review.item_id for item in menu):
         raise HTTPException(status_code=404, detail="Menu item not found.")
-    reviwes.append(review)
-    return {"massage":"Review added successfully!"}
+    reviews.append(review)
+    return {"message": "Review added successfully!"}
 
 @app.get("/reviews", response_model=List[Review])
 def get_reviews():
-    return reviwes
+    return reviews
